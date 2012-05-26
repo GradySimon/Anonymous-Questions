3ca297dc4a6400eb6af72fdec15892f1588649d4
Grady Simon <grady@gradysimon.com>
2012-05-26 17:44:41 -0400

    Wrote Question and Answer models

--- question_site/anon_questions/models.py
+++ question_site/anon_questions/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    question = models.CharField(max_length=1000)
+    ask_date = models.DateTimeField('date asked')
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question)
+    answer = models.CharField(max_length=2000)
+    answer_date = models.DateTimeField('date answered')
